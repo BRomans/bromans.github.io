f339c7519859fdd3e0c3d92060d22af330771390
Michele Romani <michele.romani.gzl0@gmail.com>
2025-08-09 16:19:55 +0200

    :sparkles: new palette for light theme, new logo, new icon, standardized theme variables across all elements.

--- js/trail.js
+++ js/trail.js
@@ -1,10 +1,13 @@
 // Mouse trail for dark mode
 let mouseTrail = [];
 const trailLength = 8;
+var drawTrail = true;
 
 document.addEventListener('mousemove', (e) => {
-    if(document.documentElement.getAttribute('data-theme') === 'dark') {
+    if(document.documentElement.getAttribute('data-theme') === 'dark' && drawTrail) {
         createTrailDot(e.pageX, e.pageY);
+    } else {
+        createTrailSquare(e.pageX, e.pageY)
     }
 });
 
@@ -15,5 +18,15 @@ function createTrailDot(x, y) {
     dot.style.top = y + 'px';
     document.body.appendChild(dot);
 
-    setTimeout(() => dot.remove(), 600);
+    setTimeout(() => dot.remove(), 300);
+}
+
+function createTrailSquare(x, y) {
+    const square = document.createElement('div');
+    square.className = 'trail-square';
+    square.style.left = x + 'px';
+    square.style.top = y + 'px';
+    document.body.appendChild(square);
+
+    setTimeout(() => square.remove(), 300);
 }
